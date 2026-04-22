7c7292c61c97d0937f72fbc7db4d5f73b3c4e8e7
CoolgamerMothman2019 <146207365+CoolgamerMothman2019@users.noreply.github.com>
2026-04-22 18:45:46 -0500

    Update injectlmaoo.js

--- injectlmaoo.js
+++ injectlmaoo.js
@@ -12,7 +12,7 @@ bar.style.fontSize = "14px";
 bar.style.zIndex = "9999";
 bar.style.display = "flex";
 document.body.appendChild(bar)
-
+alert(1);
 //if you are seeing this you lwk gotta fix the site. line 1494 in your app.js allows for me to inject html straight into your website for anyone to see. so badddddd things could happen.
 // i could inject any sort of script and have full control over the sites html, i could rewrite anything. fix it by changing it to set it to text.
 
