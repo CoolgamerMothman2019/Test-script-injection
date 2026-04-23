ca5eb4d4630371290471e3b883d0167947d54668
CoolgamerMothman2019 <146207365+CoolgamerMothman2019@users.noreply.github.com>
2026-04-22 19:40:18 -0500

    Update injectlmaoo.js
    added like an ifndef thing so it doesnt inject multiple times

--- injectlmaoo.js
+++ injectlmaoo.js
@@ -1,4 +1,9 @@
-const bar = document.createElement("div");
+let INJECTION = true; //i did like a infdef in c header files so that my script doesnt get ran multiple times and mess itself up like redefining consts.
+if (INJECTION) return;
+
+
+
+let bar = document.createElement("div");
 bar.innerText = "You lwk gotta fix this site man.....";
 bar.style.position = "fixed";
 bar.style.top = "0";
@@ -12,7 +17,7 @@ bar.style.fontSize = "14px";
 bar.style.zIndex = "9999";
 bar.style.display = "flex";
 document.body.appendChild(bar)
-//if you are seeing this you lwk gotta fix the site. line 1494 in your app.js allows for me to inject html straight into your website for anyone to see. so badddddd things could happen.
-// i could inject any sort of script and have full control over the sites html, i could rewrite anything. fix it by changing it to set it to text.
+//if you are seeing this you lwk gotta fix the site. line 1494 in your app.js allows for me to do xml injection straight into your website for anyone to see. so badddddd things could happen.
+// i could inject any sort of script and have full control over the sites html, i could rewrite anything. fix it by changing it to set it to text, not innerHTML.
 
 //you might not see this though lmao
