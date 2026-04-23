7a3c885a70d372b7819f6230331430ccc85785cf
CoolgamerMothman2019 <146207365+CoolgamerMothman2019@users.noreply.github.com>
2026-04-22 19:05:21 -0500

    Update injectlmaoo.js

--- injectlmaoo.js
+++ injectlmaoo.js
@@ -1,5 +1,5 @@
 const bar = document.createElement("div");
-bar.innerText = "You lwk gotta fix this site man.";
+bar.innerText = "You lwk gotta fix this site man.....";
 bar.style.position = "fixed";
 bar.style.top = "0";
 bar.style.left = "0";
@@ -12,7 +12,6 @@ bar.style.fontSize = "14px";
 bar.style.zIndex = "9999";
 bar.style.display = "flex";
 document.body.appendChild(bar)
-alert(1);
 //if you are seeing this you lwk gotta fix the site. line 1494 in your app.js allows for me to inject html straight into your website for anyone to see. so badddddd things could happen.
 // i could inject any sort of script and have full control over the sites html, i could rewrite anything. fix it by changing it to set it to text.
 
